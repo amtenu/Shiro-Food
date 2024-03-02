bd6408d259baddd3de4064ae85a69c6eddfe07ba
amtenu <amannov21@gmail.com>
2024-03-02 08:41:17 -0700

    Created Food entity

--- src/main/java/com/shiro/model/Category.java
+++ src/main/java/com/shiro/model/Category.java
@@ -1,29 +1,20 @@
 package com.shiro.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-public class OrderItem {
+public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-    @ManyToOne
-    private Food food;
-
-    private int quantity;
-
-    private Long totalPrice;
-
-    private List<String> recipe;
-
 
 }

199cbd64217e9d38e44e303b8a9ab10613129bd7
amtenu <amannov21@gmail.com>
2024-03-01 17:36:32 -0700

    Annotated the 'orders' field in the Customer entity with @JsonIgnore to exclude it from JSON serialization and deserialization. This field represents a one-to-many relationship with Order entities and is mapped using the @OneToMany annotation with CascadeType.ALL and mappedBy = customer

--- src/main/java/com/shiro/model/User.java
+++ src/main/java/com/shiro/model/User.java
@@ -29,7 +29,7 @@ public class User {
     private USER_ROLE role;
 
     @JsonIgnore
-    @OneToMany
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "customer")
     private List<Order> orders=new ArrayList<>();
 
     @ElementCollection

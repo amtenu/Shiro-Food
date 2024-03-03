ec14290deff260c38d39cad64d494fae80033df6
amtenu <amannov21@gmail.com>
2024-03-02 19:22:14 -0700

    Created robust JWT token validation and authentication filter in JwtTokenValidator class to enforce secure access control for API endpoints, ensuring only authenticated users with valid tokens can access protected resources.

---
+++ src/main/java/com/shiro/Config/JwsConstant.java
@@ -0,0 +1,7 @@
+package com.shiro.Config;
+
+public class JwsConstant {
+
+    public static final String  SECRET_KEY="gsgsgslabbetevsbkkkkkkbsjsbsjsbsjseoeyriga";
+    public static final String JWT_HEADER="Authorization";
+}

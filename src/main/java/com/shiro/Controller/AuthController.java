504e4aae979188bb2a67ac76dcd7fbb5929f8369
amtenu <amannov21@gmail.com>
2024-03-04 13:53:13 -0700

    Signin method created with roles

--- src/main/java/com/shiro/Controller/AuthController.java
+++ src/main/java/com/shiro/Controller/AuthController.java
@@ -1,8 +1,10 @@
 package com.shiro.Controller;
 
 import com.shiro.Config.JwtProvider;
+import com.shiro.Request.LoginRequest;
 import com.shiro.Response.AuthResponse;
 import com.shiro.model.Cart;
+import com.shiro.model.USER_ROLE;
 import com.shiro.model.User;
 import com.shiro.repository.CartRepository;
 import com.shiro.repository.UserRepository;
@@ -10,14 +12,19 @@ import com.shiro.service.CustomerUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/auth")
@@ -34,7 +41,7 @@ public class AuthController {
     @Autowired
     private CartRepository cartRepository;
 
-@PostMapping("/signup")
+    @PostMapping("/signup")
     public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws Exception {
 
         User isEmailExist = userRepository.findByEmail(user.getEmail());
@@ -68,6 +75,42 @@ public class AuthController {
         return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
 
     }
+@PostMapping("/signin")
+    public ResponseEntity<AuthResponse> siginin(@RequestBody LoginRequest req) {
+
+        String username = req.getEmail();
+        String password = req.getPassword();
 
 
+        Authentication authentication = authenticate(username, password);
+        Collection<? extends GrantedAuthority> authorities=authentication.getAuthorities();
+        String role=authorities.isEmpty() ? null : authorities.iterator().next().getAuthority();
+        String jwt = jwtProvider.generateToken(authentication);
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Registered Successfully");
+
+        authResponse.setRole(USER_ROLE.valueOf(role));
+
+
+
+        return new ResponseEntity<>(authResponse, HttpStatus.OK);
+
+
+    }
+
+    private Authentication authenticate(String username, String password) {
+        UserDetails userDetails = customerUserDetailService.loadUserByUsername(username);
+        if (userDetails == null) {
+            throw new BadCredentialsException("Invalid username ...");
+        }
+
+        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+            throw new BadCredentialsException("Wrong Password");
+        }
+        ;
+
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
 }
